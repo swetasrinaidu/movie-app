28e28abcb0d2d434b901f335b876d4010970d1fd
Sweta <97749092+swetasrinaidu@users.noreply.github.com>
2022-08-22 17:59:20 +0530

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const img_url = 'https://image.tmdb.org/t/p/w1280';
-const api_url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=04c35731a5ee918f014970082a0088b1&language=en-US&page=1';
+const api_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
-let count=0;
+const SEARCHAPI=
+      'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+var count=0;
 
 getMovies(api_url);
 async function getMovies(url){
@@ -14,11 +16,13 @@ async function getMovies(url){
 }
 function showMovies(movies){
     movies.forEach(el=>{
-         count++;
+        if(count==0) clearMovies();
+        count++;
         const {title,vote_average,release_data,overview,popularity,poster_path} = el;
-         const infoEl = document.createElement("div");
-         infoEl.id=count;
-         infoEl.classList.add("movie");
+         
+        const infoEl = document.createElement("div");
+        infoEl.id=count;
+        infoEl.classList.add("movie");
 
          const titleEl = document.createElement("h3");
          titleEl.innerHTML= `${title}`;
@@ -26,22 +30,53 @@ function showMovies(movies){
 
 
          const rateEl = document.createElement("h3");
-         rateEl.innerHTML= `Rating:${vote_average}`;
+         rateEl.innerHTML= `${vote_average}`;
          rateEl.classList.add("movie-info");
-
+         rateEl.classList.add(changeColor(vote_average));
 
          const imgEl = document.createElement("img");
          imgEl.src = IMGPATH + poster_path;
-
          
-         document.body.appendChild(infoEl);
-         document.getElementById(count).appendChild(imgEl);   
+         document.getElementById("main").appendChild(infoEl);
          document.getElementById(count).appendChild(titleEl);
+         document.getElementById(count).appendChild(imgEl);   
          document.getElementById(count).appendChild(rateEl);
-     
-    }) 
     
-    
-    
+        
+        })
+        
+        function changeColor(rate){
+            if(rate>=8) return "yellow";
+            else if(rate>=5) return "green";
+            else return "orange";
+        }
+        document.getElementById("form").addEventListener("submit",searchMovie);
+
+        function searchMovie(event){
+            event.preventDefault();
+            let search_movie = document.getElementById("search").value;
+            console.log("movie searching",search_movie);
+            if(search_movie){
+                console.log(SEARCHAPI+search_movie)
+                getMovies(SEARCHAPI+search_movie);
+                search_movie ="";
+                count=0;
+            }
+           
+        }
+function clearMovies(){   
+     var i=0;
+     while(document.getElementById("main").childNodes.length)
+     {
+       console.log(document.getElementById("main").childNodes.length);
+       console.log("yes",i)
+       console.log("id",document.getElementById("main").childNodes[0].id);
+       document.getElementById("main").removeChild(document.getElementById("main").childNodes[0]);
+       i++;
+    }
+}
 
 }
+
+
+
